893a120ba28dcb40639dea08fddb8ab45906f248
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-12 14:55:42 -0300

    agrego codigo incompleto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import HelloWorld from "./components/HelloMyFriend";
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloMyFriend.jsx
@@ -0,0 +1,9 @@
+const HelloWorld = () => {
+  return (
+    <div className="container-fluid text-bg-dark text-center p-2">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
